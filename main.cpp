70ba176b31a99c9669c642c8caac343cba0afa4a
MeeraSunu3103 <Sunu.Meera@in.bosch.com>
2024-09-30 16:54:45 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "TelCoColorCoderTestFunctions.h"
 #include "TelCoColorCoderTests_RunPairToNumberTests.h"
 #include "TelCoColorCoderTests_RunNumberToPairTests.h"
+#include "TelCoColorCoder_PrintColorCodes.h"
 
 int main() {
     RunNumberToPairTests();
     RunPairToNumberTests();
+    PrintColorCodes();
 
     return 0;
 }

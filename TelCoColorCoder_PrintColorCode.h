afcdd53b2172702cd271213eabf2b1f33d7ffd47
MeeraSunu3103 <Sunu.Meera@in.bosch.com>
2024-09-30 16:23:39 +0530

    Update TelCoColorCoder_PrintColorCode.h

--- TelCoColorCoder_PrintColorCode.h
+++ TelCoColorCoder_PrintColorCode.h
@@ -1,3 +1,10 @@
-void PrintColorCode() {
-  std::cout << "\nCode: " << "\tMajor Color: " << "\tMinor Color: ";
+void PrintColorCodes() {
+  std::cout << "\nCode\tMajor Color\tMinor Color" << std::endl;
+  for(int pairNumber = 1; pairNumber <= 25; ++pairNumber) {
+    TelCoColorCoder::ColorPair colorPair = TelCoColorCoder::GetColorFromPairNumber(pairNumber);
+    std::cout << pairNumber;
+    std::cout << "\t" << TelCoColorCoder::MajorColorNames[colorPair.getMajor()];
+    std::cout << "\t\t" << TelCoColorCoder::MinorColorNames[colorPair.getMinor()];
+    std::cout << std::endl;
+  }
 }

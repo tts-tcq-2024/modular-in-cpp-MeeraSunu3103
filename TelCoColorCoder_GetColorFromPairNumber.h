cdd8d1f4f4b9d59c6336b0d87963434dd24d1cd2
MeeraSunu3103 <Sunu.Meera@in.bosch.com>
2024-09-30 15:23:04 +0530

    Update TelCoColorCoder_GetColorFromPairNumber.h

--- TelCoColorCoder_GetColorFromPairNumber.h
+++ TelCoColorCoder_GetColorFromPairNumber.h
@@ -1,13 +1,13 @@
-#include "TelCoColorCoderDefinitions.h"
-using namespace TelCoColorCoder
-
-ColorPair GetColorFromPairNumber(int pairNumber) {
-  int zeroBasedPairNumber = pairNumber - 1;
-  MajorColor majorColor = (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
-  MinorColor minorColor = (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
-  return ColorPair(majorColor, minorColor);
-}
-
-int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
-  return major * numberOfMinorColors + minor + 1;
+namespace TelCoColorCoder
+{
+  ColorPair GetColorFromPairNumber(int pairNumber) {
+    int zeroBasedPairNumber = pairNumber - 1;
+    MajorColor majorColor = (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+    MinorColor minorColor = (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+    return ColorPair(majorColor, minorColor);
+  }
+  
+  int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
+    return major * numberOfMinorColors + minor + 1;
+  }
 }

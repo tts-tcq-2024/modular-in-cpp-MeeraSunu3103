79aef196c157c831c9786526b744cce99ac8cf1c
MeeraSunu3103 <Sunu.Meera@in.bosch.com>
2024-10-09 17:48:57 +0530

    Update and rename TelCoColorCoderTests_RunPairToNumberTests.h to TelCoColorCoderTests_RunPairToNumberTests.c

--- TelCoColorCoderTests_RunPairToNumberTests.c
+++ TelCoColorCoderTests_RunPairToNumberTests.c
@@ -1,3 +1,5 @@
+#include "TelCoColorCoderDefinitions.h"
+
 void RunPairToNumberTestsForEachPair(int& pairNumber) {
   for(int outerLoopCounter = 0; outerLoopCounter < 5; ++outerLoopCounter) {
     for(int innerLoopCounter = 0; innerLoopCounter < 5; ++innerLoopCounter) {

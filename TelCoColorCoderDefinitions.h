3b518c25642bb22566e81202e8c79064fb569777
MeeraSunu3103 <Sunu.Meera@in.bosch.com>
2024-10-09 18:06:41 +0530

    Update TelCoColorCoderDefinitions.h

--- TelCoColorCoderDefinitions.h
+++ TelCoColorCoderDefinitions.h
@@ -33,13 +33,4 @@ namespace TelCoColorCoder
                 return colorPairStr;
             }
     };
-
-    ColorPair GetColorFromPairNumber(int pairNumber);
-    int GetPairNumberFromColor(MajorColor major, MinorColor minor);
-
 }
-
-void RunNumberToPairTests();
-void RunPairToNumberTests();
-void testNumberToPair(int pairNumber, TelCoColorCoder::MajorColor expectedMajor, TelCoColorCoder::MinorColor expectedMinor);
-void testPairToNumber(TelCoColorCoder::MajorColor Major, TelCoColorCoder::MinorColor Minor, int expectedPairNumber);

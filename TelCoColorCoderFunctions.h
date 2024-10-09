d456bfc15570f34c3692a1252ea8827e4952974b
MeeraSunu3103 <Sunu.Meera@in.bosch.com>
2024-10-09 18:07:47 +0530

    Update TelCoColorCoderFunctions.h

--- TelCoColorCoderFunctions.h
+++ TelCoColorCoderFunctions.h
@@ -1,1 +1,12 @@
+#include "TelCoColorCoderDefinitions.h"
 
+namespace TelCoColorCoder
+{
+    ColorPair GetColorFromPairNumber(int pairNumber);
+    int GetPairNumberFromColor(MajorColor major, MinorColor minor);
+}
+
+void RunNumberToPairTests();
+void RunPairToNumberTests();
+void testNumberToPair(int pairNumber, TelCoColorCoder::MajorColor expectedMajor, TelCoColorCoder::MinorColor expectedMinor);
+void testPairToNumber(TelCoColorCoder::MajorColor Major, TelCoColorCoder::MinorColor Minor, int expectedPairNumber);

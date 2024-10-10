cd31ed15aabbd0cc1ddc650204edd49fb04e2324
MeeraSunu3103 <Sunu.Meera@in.bosch.com>
2024-10-10 09:59:44 +0530

    Update TelCoColorCoderFunctions.h

--- TelCoColorCoderFunctions.h
+++ TelCoColorCoderFunctions.h
@@ -1,9 +1,32 @@
 #include "TelCoColorCoderDefinitions.h"
 
-namespace TelCoColorCoder
+namespace TelCoColorCoder 
 {
+    enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+    enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+    extern const char* MajorColorNames[];
+    extern int numberOfMajorColors;
+
+    extern const char* MinorColorNames[];
+    extern int numberOfMinorColors;
+
+    class ColorPair {
+        private:
+            MajorColor majorColor;
+            MinorColor minorColor;
+        public:
+            ColorPair(MajorColor major, MinorColor minor):
+                majorColor(major), minorColor(minor) {}
+
+            MajorColor getMajor();
+            MinorColor getMinor();
+            std::string ToString();
+    };
+
     ColorPair GetColorFromPairNumber(int pairNumber);
     int GetPairNumberFromColor(MajorColor major, MinorColor minor);
+
 }
 
 void RunNumberToPairTests();

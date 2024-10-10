f9b7917c6fdd23ee90222d533d4206e6dbe2749b
MeeraSunu3103 <Sunu.Meera@in.bosch.com>
2024-10-10 10:00:02 +0530

    Update TelCoColorCoderDefinitions.h

--- TelCoColorCoderDefinitions.h
+++ TelCoColorCoderDefinitions.h
@@ -3,9 +3,6 @@
 
 namespace TelCoColorCoder 
 {
-    enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-    enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-
     const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
     int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
     
